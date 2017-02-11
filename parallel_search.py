3dfca3b09ca59d5b35d81aac55442e0c73facda5
Chaitanya Srivastav <chaitanyas@shopalyst.com>
2017-02-12 00:31:18 +0530

    some changes

--- parallel_search.py
+++ parallel_search.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import Response, jsonify
-import json
+import json, os
 import grequests
 app = Flask(__name__)
 
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def hello_world():
     searchword = request.args.get('q', '')
     resp = {'query': searchword, 'results': {'google': {}, 'duckduckgo': {}, 'twitter': {}}}
-    header = {'Authorization': 'Bearer <TOKEN>'}
+    header = {'Authorization': 'Bearer ' + os.environ['TOKEN']}
     duckduckgo_url = 'http://api.duckduckgo.com/?q={}&format=json'.format(searchword)
-    google_url = 'https://www.googleapis.com/customsearch/v1?key=GOOGLE_API_KEY&cx=017576662512468239146:omuauf_lfve&q={}'.format(searchword)
+    google_url = 'https://www.googleapis.com/customsearch/v1?key={}&cx=017576662512468239146:omuauf_lfve&q={}'.format(os.environ['GOOGLE_API_KEY'],searchword)
     twitter_url = 'https://api.twitter.com/1.1/search/tweets.json?q={}'.format(searchword)
     results = grequests.map([grequests.get(duckduckgo_url, timeout=1),
     grequests.get(google_url, timeout=1),
@@ -21,17 +21,20 @@ def hello_world():
         if result:
             rsult_json = json.loads(result.content)
             if index == 0:
-                url = rsult_json['RelatedTopics'][0]['FirstURL']
-                text = rsult_json['RelatedTopics'][0]['Text']
-                resp['results']['duckduckgo'] = {'url': url, 'text': text}
+                if rsult_json['RelatedTopics']:
+                    url = rsult_json['RelatedTopics'][0]['FirstURL']
+                    text = rsult_json['RelatedTopics'][0]['Text']
+                    resp['results']['duckduckgo'] = {'url': url, 'text': text}
             elif index == 1:
-                url = rsult_json['items'][0]['link']
-                text = rsult_json['items'][0]['title']
-                resp['results']['google'] = {'url': url, 'text': text}
+                if 'items' in rsult_json:
+                    url = rsult_json['items'][0]['link']
+                    text = rsult_json['items'][0]['title']
+                    resp['results']['google'] = {'url': url, 'text': text}
             else:
-                id = rsult_json['statuses'][0]['id_str']
-                text = rsult_json['statuses'][0]['text']
-                resp['results']['twitter'] = {'id': id, 'text': text}
+                if rsult_json['statuses']:
+                    id = rsult_json['statuses'][0]['id_str']
+                    text = rsult_json['statuses'][0]['text']
+                    resp['results']['twitter'] = {'id': id, 'text': text}
         else:
             if index == 0:
                 resp['results']['duckduckgo'] = None
